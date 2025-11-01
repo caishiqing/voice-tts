a36b2e5586d2252e6a0d388f4dc08f907d5746dc
shiqing.cai <shiqing.cai@mobvoi.com>
2025-11-01 11:47:54 +0800

    .

--- run_docker.sh
+++ run_docker.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-docker run -itd --gpus all --name voicetts \
+docker run -itd --gpus '"device=7"' --name voicetts \
 -p 8020:8020 \
 -w /app \
--v $(pwd)/data:/app/data \
--v $(pwd)/models:/app/models \
 -e HF_ENDPOINT="https://hf-mirror.com" \
 -e TZ=Asia/Shanghai \
 --network host \
 --restart unless-stopped \
 voice-tts:latest \
-python server.py --host 0.0.0.0 --port 8020 --workers 1
+python server.py --host 0.0.0.0 --port 8020 --workers 3
